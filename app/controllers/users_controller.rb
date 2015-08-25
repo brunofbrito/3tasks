e1a5daf238d0d6007813c9923fd33ef2b2452e77
Bruno Brito <brunofbrito@gmail.com>
2015-08-25 18:30:14 +0100

    Adds background chooser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,18 @@ class UsersController < Clearance::UsersController
     redirect_to edit_user_path
   end
 
+  def background
+    backgrounds = Dir.glob("app/assets/images/bground/**/*")
+    @backgrounds = backgrounds.map do |x|
+      x.slice(3..10) + x.slice(18..-1)
+    end
+    # /assets/bground/1.jpg
+  end
+
   private
 
   def edit_user_params
     params.require(:user).permit(:name, :email, :password, :avatar)
   end
-end
 
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users, only: [:edit, :update]
   root to: 'tasks#index'
+  get 'user/:id/background' => 'users#background', as: :background
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
