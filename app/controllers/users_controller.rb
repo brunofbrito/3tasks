d23511c4b39f9cc509d04e8a4315aba2d757252b
Bruno Brito <brunofbrito@gmail.com>
2015-08-26 02:39:27 +0100

    Adds Background uploader to project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,15 @@ class UsersController < Clearance::UsersController
 
   def edit
     @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.update(params[:id], edit_user_params)
-    @user.save
-    redirect_to edit_user_path
-  end
-
-  def background
     backgrounds = Dir.glob("app/assets/images/bground/**/*")
     @backgrounds = backgrounds.map do |x|
       x.slice(3..10) + x.slice(18..-1)
     end
-    @user = User.find(params[:id])
-    @user.save
-#    redirect_to background_path(current_user)
+  end
+
+  def update
+    @user = User.update(params[:id], edit_user_params)
+    redirect_to edit_user_path
   end
 
   private
@@ -26,4 +19,4 @@ class UsersController < Clearance::UsersController
     params.require(:user).permit(:name, :email, :password, :avatar, :background)
   end
 
-  end
+end
